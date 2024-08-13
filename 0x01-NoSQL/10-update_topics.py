c907e3de6c20a547116aae11cc25e433d93e3c47
Emmanuel <kcemmy03@gmail.com>
2024-08-13 19:39:47 +0100

    Function that changes all topics of a document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that changes all topics of document
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a document based on the name
+
+    Args:
+        mongo_collection: pymongo collection object.
+        name(string): the school name to update
+        topics(list of strings): list of topics
+    """
+    return mongo_collection.find_one_and_update(
+        { "name": name },
+        { "$set": { "topics": topics} }
+    );
